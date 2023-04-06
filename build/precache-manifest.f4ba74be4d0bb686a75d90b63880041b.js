eeec1a77306c505a24cc58ebe7ff6c2164d3a7f0
Berkay Akbas <berkayakbas@gmail.com>
2023-04-06 23:57:23 +0200

    add bundle

---
+++ build/precache-manifest.f4ba74be4d0bb686a75d90b63880041b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e5dc46b5dd15c3fee7006ff23ec660",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20d69841a688a8ef807a",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "efd688cf2c916def6f07",
+    "url": "/static/js/2.3d1d2612.chunk.js"
+  },
+  {
+    "revision": "20d69841a688a8ef807a",
+    "url": "/static/js/main.90ac7398.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
